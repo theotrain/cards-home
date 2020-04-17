153e59a534136f55afd23c426dc2a25b6054296a
theotrain <owen@localhost-2.local>
2020-04-17 13:19:48 -0700

    fix delay issue firefox2

--- scripts.js
+++ scripts.js
@@ -40,7 +40,7 @@ function showCard(id) {
   $('#top-card').append(new_top);
   $('#top-card').remove();
   $('#scene').append(top_card);
-  setTimeout(function() {$("#c" + id).toggleClass('is-flipped');}, 10);
+  setTimeout(function() {$("#c" + id).toggleClass('is-flipped');}, 50);
 }
 
 //disable links for testing only
