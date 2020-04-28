d4406777d2c6cf534b5359ef976d32a634a398b3
theotrain <owen@localhost-2.local>
2020-04-28 12:10:12 -0700

    new full width animation

--- scripts.js
+++ scripts.js
@@ -1,30 +1,59 @@
-$(".card").click(function(){
-  var id = parseInt($(this).attr('id')[1]);
-  showCard(id);
+// $(".card").click(function(){
+//   var id = parseInt($(this).attr('id')[1]);
+//   showCard(id);
+// });
+let cardTotal = 8
+let cardNum = 0
+let firstClick = true
+
+$("#nav-left").click(prevCard);
+$("#nav-right").click(nextCard);
+$("#scene").click(function() {
+  if (firstClick) {
+    firstClick = false;
+    $(".card").addClass("stack");
+    // nextCard();
+    setTimeout(function() {
+      $('#card-section-nav').removeClass('wide');
+      $('#card-section-controls').css("visibility", "visible");
+      $('#card-section-quotes').show();
+      nextCard();
+    }, 1000);
+  } else {
+    nextCard();
+  }
+  
 });
 
-$("#nav-left").click(function(){
-  cardNum -= 1;
-  if (cardNum < 1) { cardNum = cardTotal }
-  showCard(cardNum);
-});
-$("#nav-right").click(function(){
+function nextCard() {
   cardNum += 1;
-  if (cardNum > cardTotal) { cardNum = 1 }
+  if (cardNum > cardTotal) { cardNum = 0 }
+  if (cardNum == 0) {
+    showCard(1, true);
+    return;
+  }
   showCard(cardNum);
-});
+}
+function prevCard() {
+  cardNum -= 1;
+  if (cardNum < 0) { cardNum = cardTotal }
+  if (cardNum == 0) {
+    showCard(1, true);
+    return;
+  }
+  showCard(cardNum);
+}
 
 $(document).ready(function() {
   $('.card-quotes').hide();
-  $('#quote1').fadeIn();
-  $('#c1').toggleClass('is-flipped');
+  // $('#quote1').fadeIn();
+  // $('#c1').toggleClass('is-flipped');
   // $(".card").removeClass('is-flipped');
 });
 
-let cardTotal = 5
-let cardNum = 1
 
-function showCard(id) {
+
+function showCard(id, allFaceDown=false) {
   $(".card").removeClass('is-flipped');
   $('.card-quotes').hide();
   $('#quote' + id).fadeIn();
@@ -40,8 +69,26 @@ function showCard(id) {
   $('#top-card').append(new_top);
   $('#top-card').remove();
   $('#scene').append(top_card);
-  setTimeout(function() {$("#c" + id).toggleClass('is-flipped');}, 50);
+  if (!allFaceDown) {
+    setTimeout(function() {$("#c" + id).toggleClass('is-flipped');}, 50);
+  }
 }
+
+// function allFaceDown(oldTopCardId) {
+//   $(".card").removeClass('is-flipped');
+//   $('.card-quotes').hide();
+//   var new_top = $('#c1')[0];
+//   var old_top = $('#top-card').children()[0];
+//   var top_card = $('#top-card')[0];
+//   $('#c' + id).remove();
+//   $('#scene').append(old_top);
+//   $('#top-card').children().remove();
+//   $('#top-card').append(new_top);
+//   $('#top-card').remove();
+//   $('#scene').append(top_card);
+//   setTimeout(function() {$("#c" + id).toggleClass('is-flipped');}, 50);
+  
+// }
 
 //disable links for testing only
 $(".card-quotes a").on("click", function(event){
